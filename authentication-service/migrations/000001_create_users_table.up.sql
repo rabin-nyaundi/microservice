5ac2d398b6afe8c7bbc2628559d8ce11c384ff6e
rabin-nyaundi <nyaundirabin@gmail.com>
2022-09-15 11:14:55 +0300

    add README

---
+++ authentication-service/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE IF NOT EXISTS users(
+    id BIGSERIAL PRIMARY KEY,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email VARCHAR UNIQUE NOT NULL,
+    password_hash bytea NOT NULL,
+    active BOOLEAN NOT NULL,
+    role INTEGER,
+    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    version INTEGER NOT NULL DEFAULT 1
+)
